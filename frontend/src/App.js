7dd6841c7611aba13d127529ca708b6ed52a1fb1
HUNG-rushb <tdhtp2016@gmail.com>
2022-04-07 22:21:36 +0700

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./components/0. Layout/Footer";
+import Header from "./components/0. Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <main className="py-3">
+        <h1>Welcome to Hưng's shop!</h1>
+      </main>
+      <Footer />
+    </React.Fragment>
   );
 }
 
