ade6dead52484d5f87dfde51f51bd0c94665f2fd
HUNG-rushb <tdhtp2016@gmail.com>
2022-04-16 00:13:17 +0700

    ok

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,20 @@
+import expressAsyncHandler from "express-async-handler";
+import Product from "../models/ProductModel.js";
+
+const getProducts = expressAsyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+const getProductById = expressAsyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,34 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import Product from "../models/ProductModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-router.get(
-  "/:id",
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+// router.get(
+//   "/",
+//   expressAsyncHandler(async (req, res) => {
+//     const products = await Product.find({});
+//     res.json(products);
+//   })
+// );
 
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+// router.get(
+//   "/:id",
+//   expressAsyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id);
+
+//     if (product) {
+//       res.json(product);
+//     } else {
+//       res.status(404);
+//       throw new Error("Product not found");
+//     }
+//   })
+// );
 
 export default router;
