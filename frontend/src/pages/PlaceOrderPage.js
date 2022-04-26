83a829657226ada9848bfb3f1edddb38b95e185f
HUNG-rushb <tdhtp2016@gmail.com>
2022-04-26 19:51:51 +0700

    Create PlaceOrderPage.js

---
+++ frontend/src/pages/PlaceOrderPage.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+
+// import Message from "../components/0. Layout/Message.js";
+// import Loader from "../components/0. Layout/Loader.js";
+import FormContainer from "../components/0. Layout/FormContainer.js";
+import CheckOutSteps from "../components/Checkout/CheckOutSteps.js";
+
+const PlaceOrderPage = () => {
+  return <div>PlaceOrderPage</div>;
+};
+
+export default PlaceOrderPage;
