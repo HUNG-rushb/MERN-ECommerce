aa67e13f2b00d4708cb58eddf9b2bf7f5cba39e8
HUNG-rushb <tdhtp2016@gmail.com>
2022-05-25 09:28:00 +0700

    ok

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -29,4 +29,53 @@ const deleteProduct = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+const createProduct = expressAsyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "test",
+    price: 0,
+
+    user: req.user.id,
+    image: "/images/sample/jpg",
+    brand: "",
+    category: "",
+    countInStock: "",
+    numReviews: 0,
+    description: "",
+  });
+
+  const createdProduct = await product.save();
+
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = expressAsyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.image = image;
+    product.brand = brand;
+    product.description = description;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found!!!");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
