26bb75a57013123f1b521dea8a75c126c86f7835
HUNG-rushb <tdhtp2016@gmail.com>
2022-05-31 22:22:11 +0700

    Update OrderListPage.js

---
+++ frontend/src/pages/OrderListPage.js
@@ -0,0 +1,94 @@
+import { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+
+import Message from "../components/0. Layout/Message.js";
+import Loader from "../components/0. Layout/Loader.js";
+
+import { listUsers, deleteUser } from "../actions/userActions.js";
+
+const OrdersListPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { loading, error, users } = useSelector((state) => state.userList);
+
+  const { userInfo } = useSelector((state) => state.userLogin);
+
+  const { success: successDelete } = useSelector((state) => state.userDelete);
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      navigate("/login");
+    }
+  }, [dispatch, successDelete, userInfo]);
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) dispatch(deleteUser(id));
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th>ID</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className="fas fa-check" style={{ color: "green" }} />
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }} />
+                  )}
+                </td>
+
+                <td>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit" />
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <i className="fas fa-trash" />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default OrdersListPage;
