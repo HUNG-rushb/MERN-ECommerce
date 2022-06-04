fde486017160f8116d0838e59d1f95bb4e9088a6
HUNG-rushb <tdhtp2016@gmail.com>
2022-06-05 00:18:24 +0700

    ok

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,15 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productControllers.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+
+router.route("/:id/reviews").post(protect, createProductReview);
 
 router
   .route("/:id")
